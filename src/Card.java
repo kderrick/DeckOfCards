2bd4b27b996b9219a42a99009a1e9671f6b42b8d
Kyle <kderrick1988@gmail.com>
2018-03-18 17:42:01 -0700

    add Card class

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+
+public class Card {
+	
+	String cardSuit;
+	String cardValue;
+	public Card(String cardSuit, String cardValue) {
+		this.cardSuit = cardSuit;
+		this.cardValue = cardValue;
+	}
+
+	
+	
+}
